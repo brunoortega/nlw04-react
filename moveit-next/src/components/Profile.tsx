9d02af5aa9253cd1df706bb8ead8c918912ec9b9
Bruno Ortega <brunoortega11@gmail.com>
2021-04-08 15:43:47 -0300

    terminei c:

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,12 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css';
+
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className = {styles.profileContainer}>
             <img src="https://github.com/brunoortega.png" alt="Bruno Ortega" />
@@ -8,7 +14,7 @@ export function Profile() {
                 <strong>Bruno Ortega</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1</p>
+                    Level {level} </p>
             </div>
         </div>
     );
